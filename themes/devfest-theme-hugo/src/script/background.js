a8076e89f14219f2ee2d514bc2592fbf53ca97f5
rymbln <rymbln@gmail.com>
2019-09-05 13:35:27 +0300

    fixed background in jumbo

--- themes/devfest-theme-hugo/src/script/background.js
+++ themes/devfest-theme-hugo/src/script/background.js
@@ -7,9 +7,3 @@ new Konami(function () {
     }
 });
 
-setTimeout(function () {
-    const jumboOverlay = document.querySelector('#jumbo-overlay');
-    if (jumboOverlay) {
-        jumboOverlay.classList.add('show');
-    }
-}, 500);
